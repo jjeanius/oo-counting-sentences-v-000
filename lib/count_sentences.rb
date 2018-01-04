eeecf36c798cda12d3eb439323630e69c3368a15
Judy McGoldrick <jjeanius@verizon.net>
2018-01-04 01:36:59 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    if self.count_sentences.split('')
+      puts count_sentences
+    elsif self.count_sentences == 0
+      puts "0"
+    elsif self.split("!",",","?",".").count_sentences
+      puts count_sentences
+    end
   end
+
 end
