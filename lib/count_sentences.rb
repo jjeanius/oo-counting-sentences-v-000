3bafe4a12274a412dbf4f6de585684682e35532d
Judy McGoldrick <jjeanius@verizon.net>
2018-01-04 02:50:42 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,15 +27,9 @@ class String
   end
 
   def count_sentences
-    if self.split.count
-      return self.split.count
-    elsif self.count_sentences = 0
-      binding.pry
-      return 0
-      binding.pry
-    elsif self.split.count
-      return self
-    end
+    self.split.count
+     self.count_sentences = 0
+     self.split("").count
   end
 
 end
